d44d3b2adae4a0e206bb351c0fccf7931d5f1480
Jaret Balba <Jaretzbalba@gmail.com>
2022-05-17 11:34:38 -0700

    add scss sections

--- dist/style.js
+++ dist/style.js
@@ -7,43 +7,26 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
-/******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
 /***/ "./src/style.scss":
 /*!************************!*\
   !*** ./src/style.scss ***!
   \************************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+/***/ (() => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://neonfrontendbuild/./src/style.scss?");
+eval("throw new Error(\"Module build failed (from ./node_modules/mini-css-extract-plugin/dist/loader.js):\\nHookWebpackError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\\nSassError: Can't find stylesheet to import.\\n  ╷\\n8 │ @import './sections/section1';\\n  │         ^^^^^^^^^^^^^^^^^^^^^\\n  ╵\\n  src/style.scss 8:9  root stylesheet\\n    at tryRunOrWebpackError (/Users/jaretbalba/repos/NEON ID/NeonFrontendBuild/node_modules/webpack/lib/HookWebpackError.js:88:9)\\n    at __webpack_require_module__ (/Users/jaretbalba/repos/NEON ID/NeonFrontendBuild/node_modules/webpack/lib/Compilation.js:5051:12)\\n    at __webpack_require__ (/Users/jaretbalba/repos/NEON ID/NeonFrontendBuild/node_modules/webpack/lib/Compilation.js:5008:18)\\n    at /Users/jaretbalba/repos/NEON ID/NeonFrontendBuild/node_modules/webpack/lib/Compilation.js:5079:20\\n    at symbolIterator (/Users/jaretbalba/repos/NEON ID/NeonFrontendBuild/node_modules/neo-async/async.js:3485:9)\\n    at done (/Users/jaretbalba/repos/NEON ID/NeonFrontendBuild/node_modules/neo-async/async.js:3527:9)\\n    at Hook.eval [as callAsync] (eval at create (/Users/jaretbalba/repos/NEON ID/NeonFrontendBuild/node_modules/tapable/lib/HookCodeFactory.js:33:10), <anonymous>:15:1)\\n    at Hook.CALL_ASYNC_DELEGATE [as _callAsync] (/Users/jaretbalba/repos/NEON ID/NeonFrontendBuild/node_modules/tapable/lib/Hook.js:18:14)\\n    at /Users/jaretbalba/repos/NEON ID/NeonFrontendBuild/node_modules/webpack/lib/Compilation.js:4986:43\\n    at symbolIterator (/Users/jaretbalba/repos/NEON ID/NeonFrontendBuild/node_modules/neo-async/async.js:3482:9)\\n-- inner error --\\nError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\\nSassError: Can't find stylesheet to import.\\n  ╷\\n8 │ @import './sections/section1';\\n  │         ^^^^^^^^^^^^^^^^^^^^^\\n  ╵\\n  src/style.scss 8:9  root stylesheet\\n    at Object.<anonymous> (/Users/jaretbalba/repos/NEON ID/NeonFrontendBuild/node_modules/css-loader/dist/cjs.js!/Users/jaretbalba/repos/NEON ID/NeonFrontendBuild/node_modules/sass-loader/dist/cjs.js!/Users/jaretbalba/repos/NEON ID/NeonFrontendBuild/src/style.scss:1:7)\\n    at /Users/jaretbalba/repos/NEON ID/NeonFrontendBuild/node_modules/webpack/lib/javascript/JavascriptModulesPlugin.js:441:11\\n    at Hook.eval [as call] (eval at create (/Users/jaretbalba/repos/NEON ID/NeonFrontendBuild/node_modules/tapable/lib/HookCodeFactory.js:19:10), <anonymous>:7:1)\\n    at Hook.CALL_DELEGATE [as _call] (/Users/jaretbalba/repos/NEON ID/NeonFrontendBuild/node_modules/tapable/lib/Hook.js:14:14)\\n    at /Users/jaretbalba/repos/NEON ID/NeonFrontendBuild/node_modules/webpack/lib/Compilation.js:5053:39\\n    at tryRunOrWebpackError (/Users/jaretbalba/repos/NEON ID/NeonFrontendBuild/node_modules/webpack/lib/HookWebpackError.js:83:7)\\n    at __webpack_require_module__ (/Users/jaretbalba/repos/NEON ID/NeonFrontendBuild/node_modules/webpack/lib/Compilation.js:5051:12)\\n    at __webpack_require__ (/Users/jaretbalba/repos/NEON ID/NeonFrontendBuild/node_modules/webpack/lib/Compilation.js:5008:18)\\n    at /Users/jaretbalba/repos/NEON ID/NeonFrontendBuild/node_modules/webpack/lib/Compilation.js:5079:20\\n    at symbolIterator (/Users/jaretbalba/repos/NEON ID/NeonFrontendBuild/node_modules/neo-async/async.js:3485:9)\\n\\nGenerated code for /Users/jaretbalba/repos/NEON ID/NeonFrontendBuild/node_modules/css-loader/dist/cjs.js!/Users/jaretbalba/repos/NEON ID/NeonFrontendBuild/node_modules/sass-loader/dist/cjs.js!/Users/jaretbalba/repos/NEON ID/NeonFrontendBuild/src/style.scss\\n1 | throw new Error(\\\"Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\\\\nSassError: Can't find stylesheet to import.\\\\n  ╷\\\\n8 │ @import './sections/section1';\\\\n  │         ^^^^^^^^^^^^^^^^^^^^^\\\\n  ╵\\\\n  src/style.scss 8:9  root stylesheet\\\");\");\n\n//# sourceURL=webpack://neonfrontendbuild/./src/style.scss?");
 
 /***/ })
 
 /******/ 	});
 /************************************************************************/
-/******/ 	// The require scope
-/******/ 	var __webpack_require__ = {};
-/******/ 	
-/************************************************************************/
-/******/ 	/* webpack/runtime/make namespace object */
-/******/ 	(() => {
-/******/ 		// define __esModule on exports
-/******/ 		__webpack_require__.r = (exports) => {
-/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-/******/ 			}
-/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/************************************************************************/
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
-/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
 /******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/style.scss"](0, __webpack_exports__, __webpack_require__);
+/******/ 	__webpack_modules__["./src/style.scss"]();
 /******/ 	
 /******/ })()
 ;
